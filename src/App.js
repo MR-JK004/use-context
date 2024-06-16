e52fd56b9273d91c6b8a0ba07965795c8262931a
MR-JK004 <jitendermaruthu2004@gmail.com>
2024-06-16 21:29:59 +0530

    Updated Style

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ const App = () => {
 
   return (
     <Container className="App">
+    <div style={{width:'70%'}}>
       {products.map(product => (
         <ProductCard key={product.id} product={product} />
       ))}
-      <div className="product-summary">
+      </div>
+      <div className="product-summary" style={{width:'28%'}}>
         <Row>
           <Col md={6}></Col>
-          <Col md={3} className="text-right">
+          <Col md={3} className="text-right heading">
             <p>SUBTOTAL :</p>
             <p>SHIPPING :</p>
           </Col>
@@ -28,7 +30,7 @@ const App = () => {
           <Col md={3} className="text-right total">
             <h5>TOTAL :</h5>
           </Col>
-          <Col md={3} className="text-right">
+          <Col md={3} className="text-right heading">
             <h5>${total.toFixed(2)}</h5>
           </Col>
         </Row>
